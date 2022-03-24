ddcfe1a9ab098f7327620fac0f9de4725684ed0a
SrBiolizard <j.molina@unillanos.edu.co>
2022-03-24 17:22:11 -0500

    Hola i

--- Database/powercards_persona.sql
+++ Database/powercards_persona.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.26, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
 --
 -- Host: localhost    Database: powercards
 -- ------------------------------------------------------
--- Server version	8.0.26
+-- Server version	8.0.28
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -37,7 +37,7 @@ CREATE TABLE `persona` (
 
 LOCK TABLES `persona` WRITE;
 /*!40000 ALTER TABLE `persona` DISABLE KEYS */;
-INSERT INTO `persona` VALUES ('admin','admin','administrador'),('DAVID','','jugador'),('Orion','','jugador'),('pi','','jugador');
+INSERT INTO `persona` VALUES ('admin','admin','administrador'),('DAVID','','jugador'),('hola si','','jugador'),('Orion','','jugador'),('pi','','jugador'),('sexo','','jugador'),('sexo1','','jugador');
 /*!40000 ALTER TABLE `persona` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -50,4 +50,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-03-23 20:56:09
+-- Dump completed on 2022-03-24 17:19:24
